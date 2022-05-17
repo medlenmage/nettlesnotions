5299c61e6286f0c201955ea9e26ebc649a7dc2e5
Joshua Medlen <joshua246@gmail.com>
2022-05-17 12:06:32 -0500

    adding social media links to landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.scss';
+import '../../Styles/index.scss';
 
 export const LandingPage = () => <main className="container--landing">
             <div className="landing" id="nettles-landing">
                 <div className="landing-content">
                     <h1 className="landing-header">Nettles and Notions</h1>
                     <p className="introduction">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-                        ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-                        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-                        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-                        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
-                        ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
-                        vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                        Hello and Welcome to Nettles and Notions. My name is Christian Alsider and, this blog site is here for my general musings and thoughts on
+                        naturistic life style as well as a place for my photography of nature.
                     </p>
                     <Link id="blog-entry" to="/nettlescorner">Nettles Corner</Link>
+                    <hr></hr>
+                    <i className="fa-brands fa-instagram"></i>
                 </div>
             </div>
         </main>;
